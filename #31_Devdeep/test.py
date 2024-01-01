02c00146af3158e191216dd809db084036a37ac3
Haiirohito <haiirohito@gmail.com>
2024-01-02 04:55:04 +0530

    started to work on the basic pattersn of the qr

---
+++ #31_Devdeep/test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+width = 7  # Assuming the width is 7 for the example
+
+# Create arrays with continuous values
+x_bottom_left = np.linspace(0, width, num=2, endpoint=False)
+y_bottom_left = np.linspace(0, width, num=2, endpoint=False)
+
+print("x_bottom_left:", x_bottom_left)
+print("y_bottom_left:", y_bottom_left)
