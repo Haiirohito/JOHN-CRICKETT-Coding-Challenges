4d57ae6f5ed705b7e257a7725309b03ffdc47f73
Haiirohito <haiirohito@gmail.com>
2024-01-02 17:58:56 +0530

    implemented everyting to the main function

--- #31_Devdeep/basic_plot.py
+++ #31_Devdeep/basic_plot.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+from square import Square
+
 class BasicPlot:
     def __init__(self, size=8, show=True) -> None:
         self.size = size
         self.show = show
 
-    def plot(self, a, n):
+    def plot(self, a):
         fig = plt.figure(figsize=(self.size, self.size))
         ax = fig.add_subplot(1, 1, 1)
 
@@ -17,22 +19,15 @@ class BasicPlot:
         ax.set_yticks(interval)
         ax.grid(color='k', linestyle='--', linewidth=0.5)
 
-        # Plot a square in the top-left corner
-        top_left_square = plt.Rectangle((0, 0), n, n, edgecolor='r', facecolor='r')
-        ax.add_patch(top_left_square)
-
-        # Plot a square in the bottom-left corner
-        bottom_left_square = plt.Rectangle((0, a-a//3), n, n, edgecolor='b', facecolor='b')
-        ax.add_patch(bottom_left_square)
-        
-        # Plot a square in the top-right corner
-        bottom_left_square = plt.Rectangle((a-a//3, a-a//3), n, n, edgecolor='g', facecolor='g')
-        ax.add_patch(bottom_left_square)
+        # Plot a square with an inner square in the top-left corner
+        square1 = Square(ax, a//3, (0, 0), 'k')
+        square1.draw()
+        # Plot a square with an inner square in the bottom-left corner
+        square2 = Square(ax, a//3, (0, a-a//3), 'k')
+        square2.draw()
+        # Plot a square with an inner square in the top-right corner
+        square3 = Square(ax, a//3, (a-a//3, a-a//3), 'k')
+        square3.draw()
 
         if self.show:
             plt.show()
-
-if __name__ == "__main__":
-    # Example usage
-    plotter = BasicPlot()
-    plotter.plot(22, 7)
--- #31_Devdeep/main.py
+++ #31_Devdeep/main.py
@@ -1,1 +1,6 @@
-print("hello world")
+from basic_plot import BasicPlot
+from square import Square
+
+if __name__ == '__main__':
+    ploter = BasicPlot()
+    ploter.plot(21)
