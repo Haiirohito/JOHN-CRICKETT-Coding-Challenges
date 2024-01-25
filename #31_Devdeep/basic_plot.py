2ea264d76d4b4905dcf6498964c3b4bc11eec7ea
Haiirohito <haiirohito@gmail.com>
2024-01-25 11:04:02 +0530

    new

--- #31_Devdeep/basic_plot.py
+++ #31_Devdeep/basic_plot.py
@@ -8,7 +8,7 @@ class BasicPlot:
         self.size = size
         self.show = show
 
-    def plot(self, a, size_factor):
+    def plot(self, a):
         fig = plt.figure(figsize=(self.size, self.size))
         ax = fig.add_subplot(1, 1, 1)
 
@@ -20,15 +20,19 @@ class BasicPlot:
         
         plt.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
 
+# makeing the  recognition squares
         # Plot a square with an inner square in the top-left corner
-        square1 = Square(ax, a//size_factor, (0, 0), 'k')
+        square1 = Square(ax, 7, (0, 0), 'k')
         square1.draw()
         # Plot a square with an inner square in the bottom-left corner
-        square2 = Square(ax, a//size_factor, (0, a-a//size_factor), 'k')
+        square2 = Square(ax, 7, (0, a-7), 'k')
         square2.draw()
         # Plot a square with an inner square in the top-right corner
-        square3 = Square(ax, a//size_factor, (a-a//size_factor, a-a//size_factor), 'k')
+        square3 = Square(ax, 7, (a-7, a-7), 'k')
         square3.draw()
+        
+# making the orientation squares
+
 
         if self.show:
             plt.show()
--- #31_Devdeep/main.py
+++ #31_Devdeep/main.py
@@ -1,7 +1,6 @@
 from basic_plot import BasicPlot
 
 if __name__ == '__main__':
-    qr_size = int(input("enter the size of the QR : "))
     size_factor = int(input("enter the size factor for the recog. pattern : "))
     ploter = BasicPlot()
-    ploter.plot(qr_size, size_factor)
+    ploter.plot(size_factor)
